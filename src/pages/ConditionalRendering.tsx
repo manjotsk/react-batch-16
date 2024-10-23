441c8ebfe728dbd876a1b369db015accb69775fc
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-10-23 16:06:54 +0530

    refactor: Update ConditionalRendering component to include additional rendering options
    - Add a button to toggle between using a ternary operator and an and operator for conditional rendering
    - Update the component to render different elements based on the selected option

--- src/pages/ConditionalRendering.tsx
+++ src/pages/ConditionalRendering.tsx
@@ -1,16 +1,44 @@
-import React from "react";
-
-const redFlag = !true;
+import React, { useState } from "react";
 
 const ConditionalRendering = () => {
+  const [flag, setFlag] = useState(false);
   return (
     <div>
-      using ternary operator
-      {redFlag ? (
+      1. using ternary operator{" "}
+      <button
+        onClick={() => {
+          setFlag((previousFlag) => {
+            return !previousFlag;
+          });
+        }}
+      >
+        switch
+      </button>
+      {flag ? (
         <div className="bg-red-400 text-white font-bold text-5xl text-center rounded-full p-10">
           hello
         </div>
       ) : (
+        <div className="bg-blue-400 text-white font-bold text-5xl text-center rounded-full p-10">
+          hello
+        </div>
+      )}
+      2. using and operator{" "}
+      <button
+        onClick={() => {
+          setFlag((previousFlag) => {
+            return !previousFlag;
+          });
+        }}
+      >
+        switch
+      </button>
+      {flag && (
+        <div className="bg-red-400 text-white font-bold text-5xl text-center rounded-full p-10">
+          hello
+        </div>
+      )}
+      {!flag && (
         <div className="bg-blue-400 text-white font-bold text-5xl text-center rounded-full p-10">
           hello
         </div>
