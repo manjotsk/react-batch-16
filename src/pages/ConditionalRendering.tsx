067d1f190c272615b541ccab1a714be113620ac5
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-10-23 16:26:43 +0530

    refactor: Update ConditionalRendering component to include additional rendering options and input fields

--- src/pages/ConditionalRendering.tsx
+++ src/pages/ConditionalRendering.tsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 
 const ConditionalRendering = () => {
   const [flag, setFlag] = useState(false);
+  const [text, setText] = useState("");
+  const [text1, setText1] = useState("");
+
   return (
     <div>
       1. using ternary operator{" "}
@@ -43,6 +46,26 @@ const ConditionalRendering = () => {
           hello
         </div>
       )}
+      <br />
+      3. using or operator <br />
+      <br />
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+        placeholder="Enter Text"
+      />
+      <input
+        type="text"
+        value={text1}
+        onChange={(e) => {
+          setText1(e.target.value);
+        }}
+        placeholder="Enter Text1"
+      />
+      <h1 className="text-5xl">{text1 || text || "No Text Entered"}</h1>
     </div>
   );
 };
