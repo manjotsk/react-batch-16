a70e19cd74a2efd55990c1755293d9096f147d0b
Manjot Singh <manjot.kalsi@simbaquartz.com>
2024-11-29 16:52:14 +0530

    feat: Add Yup validation library and implement basic validation schemas

---
+++ yup.js
@@ -0,0 +1,25 @@
+import * as Yup from "yup";
+
+const stringValidation = Yup.string().strict().min(3).max(10);
+const numberValidation = Yup.number().min(10).max(99).required();
+const emailValidation = Yup.string().email();
+const userObjectValidation = Yup.object({
+  name: Yup.string().strict().required().min(3),
+  email: Yup.string().email().required(),
+  phone: Yup.string().optional().length(10),
+});
+
+let validation = stringValidation.validateSync("hello");
+console.log(validation);
+
+validation = numberValidation.validateSync(10);
+console.log(validation);
+
+validation = emailValidation.validateSync("manjot@gmail.com");
+console.log(validation);
+
+validation = userObjectValidation.validateSync({
+  name: "Joe",
+  email: "jo@jojo.com",
+});
+console.log(validation);
